ddaa8936256877c16e4d5ab12817e6a7be2ef9ce
niercin <niercin@tulpar.ercinhome>
2024-03-03 00:17:26 +0300

    Some text extraction and LLM parameters are carried to UI for easier testing.

--- LLMs/QA_app/pdf_qa.py
+++ LLMs/QA_app/pdf_qa.py
@@ -16,10 +16,19 @@ from transformers import AutoTokenizer
 import torch
 import os
 import re
+import shutil
 
 import sys
 sys.path.insert(1, '../../../t.ext2.tractor')
 from pageextractor import process_pdf_file
+import utils
+
+def document_array_to_txts(texts, out_dir):
+    os.makedirs(out_dir)
+    for page_no, page in enumerate(texts):
+        with open(out_dir / ("page_" + str(page_no).zfill(5) + ".txt"), 'w') as text_file:
+            text_file.write(''.join(page.page_content))
+    print('Text files are created in: ' + str(out_dir))
 
 class PdfQA:
     def __init__(self,config:dict = {}):
@@ -44,26 +53,26 @@ class PdfQA:
         return HuggingFaceEmbeddings(model_name=EMB_SBERT_MPNET_BASE, model_kwargs={"device": device})    
     
     @classmethod
-    def create_flan_t5_xxl(cls, load_in_8bit=False):
+    def create_flan_t5_xxl(cls, max_len, max_new_tokens, load_in_8bit=False):
         # Local flan-t5-xxl with 8-bit quantization for inference
         # Wrap it in HF pipeline for use with LangChain
         return pipeline(
             task="text2text-generation",
             model="google/flan-t5-xxl",
-            max_new_tokens=200,
-            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": 512, "temperature": 0.}
+            max_new_tokens=max_new_tokens,
+            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": max_len, "temperature": 0.}
         )
     @classmethod
-    def create_flan_t5_xl(cls, load_in_8bit=False):
+    def create_flan_t5_xl(cls, max_len, max_new_tokens, load_in_8bit=False):
         return pipeline(
             task="text2text-generation",
             model="google/flan-t5-xl",
-            max_new_tokens=200,
-            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": 512, "temperature": 0.}
+            max_new_tokens=max_new_tokens,
+            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": max_len, "temperature": 0.}
         )
     
     @classmethod
-    def create_flan_t5_small(cls, load_in_8bit=False):
+    def create_flan_t5_small(cls, max_len, max_new_tokens, load_in_8bit=False):
         # Local flan-t5-small for inference
         # Wrap it in HF pipeline for use with LangChain
         model="google/flan-t5-small"
@@ -72,11 +81,11 @@ class PdfQA:
             task="text2text-generation",
             model=model,
             tokenizer = tokenizer,
-            max_new_tokens=100,
-            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": 512, "temperature": 0.}
+            max_new_tokens=max_new_tokens,
+            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": max_len, "temperature": 0.}
         )
     @classmethod
-    def create_flan_t5_base(cls, load_in_8bit=False):
+    def create_flan_t5_base(cls, max_len, max_new_tokens, load_in_8bit=False):
         # Wrap it in HF pipeline for use with LangChain
         model="google/flan-t5-base"
         tokenizer = AutoTokenizer.from_pretrained(model)
@@ -84,11 +93,11 @@ class PdfQA:
             task="text2text-generation",
             model=model,
             tokenizer = tokenizer,
-            max_new_tokens=100,
-            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": 512, "temperature": 0.}
+            max_new_tokens=max_new_tokens,
+            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": max_len, "temperature": 0.}
         )
     @classmethod
-    def create_flan_t5_large(cls, load_in_8bit=False):
+    def create_flan_t5_large(cls, max_len, max_new_tokens, load_in_8bit=False):
         # Wrap it in HF pipeline for use with LangChain
         model="google/flan-t5-large"
         tokenizer = AutoTokenizer.from_pretrained(model)
@@ -96,33 +105,32 @@ class PdfQA:
             task="text2text-generation",
             model=model,
             tokenizer = tokenizer,
-            max_new_tokens=100,
-            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": 512, "temperature": 0.}
+            max_new_tokens=max_new_tokens,
+            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": max_len, "temperature": 0.}
         )
     @classmethod
-    def create_fastchat_t5_xl(cls, load_in_8bit=False):
+    def create_fastchat_t5_xl(cls, max_len, max_new_tokens, load_in_8bit=False):
         return pipeline(
             task="text2text-generation",
             model = "lmsys/fastchat-t5-3b-v1.0",
-            max_new_tokens=100,
-            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": 512, "temperature": 0.}
+            max_new_tokens=max_new_tokens,
+            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": max_len, "temperature": 0.}
         )
     
     @classmethod
-    def create_falcon_instruct_small(cls, load_in_8bit=False):
+    def create_falcon_instruct_small(cls, max_len, max_new_tokens, load_in_8bit=False):
         model = "tiiuae/falcon-7b-instruct"
-
         tokenizer = AutoTokenizer.from_pretrained(model)
         hf_pipeline = pipeline(
                 task="text-generation",
                 model = model,
                 tokenizer = tokenizer,
                 trust_remote_code = True,
-                max_new_tokens=100,
+                max_new_tokens=max_new_tokens,
                 model_kwargs={
                     "device_map": "auto", 
                     "load_in_8bit": load_in_8bit, 
-                    "max_length": 512, 
+                    "max_length": max_len, 
                     "temperature": 0.01,
                     "torch_dtype":torch.bfloat16,
                     }
@@ -187,7 +195,10 @@ class PdfQA:
             ## Load from the persist db
             self.vectordb = Chroma(persist_directory=persist_directory, embedding_function=self.embedding)
         elif pdf_path and os.path.exists(pdf_path):
+            print('Processing: ' + pdf_path)
             text_extraction_method = self.config.get("text_ext", None)
+            print('Text Extraction: ' + text_extraction_method)
+
             if text_extraction_method == TEXTEXT_DEFAULT:
                 ## 1. Extract the documents
                 loader = PDFPlumberLoader(pdf_path)
@@ -195,25 +206,43 @@ class PdfQA:
                 ## 2. Split the texts
                 text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
                 texts = text_splitter.split_documents(documents)
-                # text_splitter = TokenTextSplitter(chunk_size=100, chunk_overlap=10, encoding_name="cl100k_base")  # This the encoding for text-embedding-ada-002
-                text_splitter = TokenTextSplitter(chunk_size=100, chunk_overlap=10)  # This the encoding for text-embedding-ada-002
+                text_splitter = TokenTextSplitter(chunk_size=self.config.get("chunk_size", 100),
+                                                  chunk_overlap=self.config.get("chunk_overlap", 10))
                 texts = text_splitter.split_documents(texts)
+
+                ## niercin DEBUGGING STUFF
+                #test_out_dir = utils.get_parent_directory(pdf_path) / utils.get_filename_wo_ext(pdf_path)
+                #document_array_to_txts(texts, test_out_dir)
+                #########
+
                 ## 3. Create Embeddings and add to chroma store
                 ##TODO: Validate if self.embedding is not None
                 self.vectordb = Chroma.from_documents(documents=texts, embedding=self.embedding, persist_directory=persist_directory)
+                print('Done Processing!')
             elif text_extraction_method == TEXTEXT_EXTENDED:
-                print('Processing: ' + pdf_path)
+                # Generate texts from the PDF and place
+                # them under out_dir
                 out_dir = process_pdf_file(pdf_path)
                 print('Text files are created in: ' + str(out_dir))
+                # Create DirectoryLoader to get texts from our_dir via TextLoader
                 loader = DirectoryLoader(str(out_dir), glob="**/page_*.txt", use_multithreading=True, loader_cls=TextLoader)
-                texts = loader.load()
+                # Text splitter for creating chunks of texts with given length
+                text_splitter = TokenTextSplitter(chunk_size=self.config.get("chunk_size", 100),
+                                                  chunk_overlap=self.config.get("chunk_overlap", 10))
+                texts = text_splitter.split_documents(loader.load())
                 self.vectordb = Chroma.from_documents(documents=texts, embedding=self.embedding, persist_directory=persist_directory)
+                try:
+                    os.remove(pdf_path)
+                    shutil.rmtree(out_dir)
+                except OSError:
+                    pass
+                print('Done Processing!')
             else:
-                raise ValueError("Unknown Text Extraction Method")
+                raise ValueError("Unknown Text Extraction Method: " + text_extraction_method)
         else:
             raise ValueError("NO PDF found")
 
-    def retreival_qa_chain(self):
+    def retrieval_qa_chain(self):
         """
         Creates retrieval qa chain using vectordb as retrivar and LLM to complete the prompt
         """
@@ -244,7 +273,6 @@ class PdfQA:
         """
         Answer the question
         """
-
         answer_dict = self.qa({"query":question,})
         print(answer_dict)
         answer = answer_dict["result"]
--- LLMs/QA_app/streamlit_app_blog.py
+++ LLMs/QA_app/streamlit_app_blog.py
@@ -19,20 +19,22 @@ if "pdf_qa_model" not in st.session_state:
 
 ## To cache resource across multiple session 
 @st.cache_resource
-def load_llm(llm,load_in_8bit):
+def load_llm(llm, max_len, max_new_tokens, load_in_8bit):
 
     if llm == LLM_OPENAI_GPT35:
         pass
     elif llm == LLM_FLAN_T5_SMALL:
-        return PdfQA.create_flan_t5_small(load_in_8bit)
+        return PdfQA.create_flan_t5_small(max_len, max_new_tokens, load_in_8bit)
     elif llm == LLM_FLAN_T5_BASE:
-        return PdfQA.create_flan_t5_base(load_in_8bit)
+        return PdfQA.create_flan_t5_base(max_len, max_new_tokens, load_in_8bit)
     elif llm == LLM_FLAN_T5_LARGE:
-        return PdfQA.create_flan_t5_large(load_in_8bit)
+        return PdfQA.create_flan_t5_large(max_len, max_new_tokens, load_in_8bit)
+    elif llm == LLM_FLAN_T5_XL:
+        return PdfQA.create_flan_t5_xl(max_len, max_new_tokens, load_in_8bit)
     elif llm == LLM_FASTCHAT_T5_XL:
-        return PdfQA.create_fastchat_t5_xl(load_in_8bit)
+        return PdfQA.create_fastchat_t5_xl(max_len, max_new_tokens, load_in_8bit)
     elif llm == LLM_FALCON_SMALL:
-        return PdfQA.create_falcon_instruct_small(load_in_8bit)
+        return PdfQA.create_falcon_instruct_small(max_len, max_new_tokens, load_in_8bit)
     else:
         raise ValueError("Invalid LLM setting")
 
@@ -51,10 +53,14 @@ def load_emb(emb):
 st.title("PDF Q&A (Self hosted LLMs)")
 
 with st.sidebar:
-    txt_ext = st.radio("**Select Text Extraction**", [TEXTEXT_DEFAULT, TEXTEXT_EXTENDED], index=1)
+    txt_ext = st.radio("**Select Text Extraction**", [TEXTEXT_DEFAULT, TEXTEXT_EXTENDED], index=0)
     emb = st.radio("**Select Embedding Model**", [EMB_INSTRUCTOR_XL, EMB_SBERT_MPNET_BASE,EMB_SBERT_MINILM],index=1)
     llm = st.radio("**Select LLM Model**", [LLM_FASTCHAT_T5_XL, LLM_FLAN_T5_SMALL,LLM_FLAN_T5_BASE,LLM_FLAN_T5_LARGE,LLM_FLAN_T5_XL,LLM_FALCON_SMALL],index=2)
     load_in_8bit = st.radio("**Load 8 bit**", [True, False],index=1)
+    chunk_size = st.number_input('Chunk Size', min_value=100, max_value=4096, value=100, format='%d', step=1)
+    chunk_overlap = st.number_input('Chunk Overlap', min_value=0, max_value=100, value=10, format='%d', step=1)
+    max_length = st.number_input('Max Length', min_value=100, max_value=4096, value=512, format='%d', step=1)
+    max_new_tokens = st.number_input('Max New Tokens', min_value=100, max_value=4096, value=100, format='%d', step=1)
     pdf_file = st.file_uploader("**Upload PDF**", type="pdf")
 
     if st.button("Submit") and pdf_file is not None:
@@ -67,10 +73,12 @@ with st.sidebar:
                     "pdf_path": str(tmp_path),
                     "embedding": emb,
                     "llm": llm,
-                    "load_in_8bit": load_in_8bit
+                    "load_in_8bit": load_in_8bit,
+                    "chunk_size": chunk_size,
+                    "chunk_overlap": chunk_overlap
                 }
                 st.session_state["pdf_qa_model"].embedding = load_emb(emb)
-                st.session_state["pdf_qa_model"].llm = load_llm(llm,load_in_8bit)        
+                st.session_state["pdf_qa_model"].llm = load_llm(llm, max_length, max_new_tokens, load_in_8bit)        
                 st.session_state["pdf_qa_model"].init_embeddings()
                 st.session_state["pdf_qa_model"].init_models()
                 st.session_state["pdf_qa_model"].vector_db_pdf()
@@ -80,7 +88,7 @@ question = st.text_input('Ask a question', 'What is this document?')
 
 if st.button("Answer"):
     try:
-        st.session_state["pdf_qa_model"].retreival_qa_chain()
+        st.session_state["pdf_qa_model"].retrieval_qa_chain()
         answer = st.session_state["pdf_qa_model"].answer_query(question)
         st.write(f"{answer}")
     except Exception as e:
